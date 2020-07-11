5ef4d35a39f296b8f0b53c0c975de3b2ccdb6f5d
Giang <trgiang1998@gmail.com>
2020-07-11 16:03:26 +0700

    add dummy expired with animation

--- Assets/Scripts/Dummy/DummyScript.cs
+++ Assets/Scripts/Dummy/DummyScript.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 public class DummyScript : MonoBehaviour
 {
     private WorkerControl _interactableWorker = null;
+    public float _activeTimer;
+    private Animator _anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _anim = GetComponent<Animator>();
+
+        //count down till defloat
+        InvokeRepeating("DecreaseActiveTimer", 1.0f, 1.0f);
     }
 
     // Update is called once per frame
@@ -20,6 +26,20 @@ public class DummyScript : MonoBehaviour
         if (_interactableWorker != null)
             if (_interactableWorker.IsSlacking())
                 _interactableWorker.Deslack();
+
+
+        //check the active timer
+        if (_activeTimer <= 0) {
+            _anim.SetTrigger("deactivate");
+            //destroy object after done playing animation
+            Destroy(gameObject, _anim.GetCurrentAnimatorStateInfo(0).length);
+            
+        }  
+    }
+
+    private void DecreaseActiveTimer() {
+        _activeTimer--;
+        Debug.Log(_activeTimer);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,7 +47,6 @@ public class DummyScript : MonoBehaviour
         if (collision.gameObject.tag == "Worker")
         {
             _interactableWorker = collision.gameObject.GetComponent<WorkerControl>();
-            Debug.Log("worker in trigger zone");
         }
     }
 
