8f085e19793c97c172cac3220d61a7b20ea55ee2
dphuong98 <32475252+dphuong98@users.noreply.github.com>
2020-07-11 10:01:02 +0700

    Added 2d topdown template

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(UnitMovement))]
+public class PlayerControl : MonoBehaviour
+{
+    private float _moveX;
+    private float _moveY;
+
+    //Components
+    private UnitMovement _move;
+    private Animator _anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _move = GetComponent<UnitMovement>();
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveX = Input.GetAxisRaw("Horizontal");
+        _moveY = Input.GetAxisRaw("Vertical");
+
+        if (!(_moveX == 0 && _moveY == 0))
+        {
+            _anim.SetFloat("FaceX", _moveX);
+            _anim.SetFloat("FaceY", _moveY);
+            _anim.SetBool("Walking", true);
+        }
+        else _anim.SetBool("Walking", false);
+
+        _move.Walk(_moveX, _moveY);
+    }
+}
---
+++ Assets/Scripts/Player/UnitMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMovement : MonoBehaviour
+{
+    private float _speed = 5;
+    private float _diagSlow = 0.7f;
+
+    public float Speed {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+
+    public void Walk(float moveX, float moveY)
+    {
+        Vector3 movement = new Vector3(moveX, moveY, 0);
+        transform.Translate(movement * _speed * (moveX != 0 && moveY != 0 ? _diagSlow : 1) * Time.deltaTime);
+    }
+}
