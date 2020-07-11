193872ba3150b218fbdbea5cc663da70473e5528
dphuong98 <32475252+dphuong98@users.noreply.github.com>
2020-07-11 16:21:49 +0700

    Player can't collide with dog
    -Unit now move using MovePosition() instead of Translate()

--- Assets/Scripts/Player/UnitMovement.cs
+++ Assets/Scripts/Player/UnitMovement.cs
@@ -7,14 +7,25 @@ public class UnitMovement : MonoBehaviour
     private float _speed = 5;
     private float _diagSlow = 0.7f;
 
+    //Component
+    private Rigidbody2D _rb;
+
     public float Speed {
         get { return _speed; }
         set { _speed = value; }
     }
 
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
     public void Walk(float moveX, float moveY)
     {
-        Vector3 movement = new Vector3(moveX, moveY, 0);
-        transform.Translate(movement * _speed * (moveX != 0 && moveY != 0 ? _diagSlow : 1) * Time.deltaTime);
+        Vector2 movement = new Vector2(moveX, moveY);
+        _rb.MovePosition(_rb.position + movement * (moveX != 0 && moveY != 0 ? _diagSlow : 1) * _speed * Time.fixedDeltaTime);
+
+        //Vector3 movement = new Vector3(moveX, moveY, 0);
+        //transform.Translate(movement * _speed * (moveX != 0 && moveY != 0 ? _diagSlow : 1) * Time.deltaTime);
     }
 }
