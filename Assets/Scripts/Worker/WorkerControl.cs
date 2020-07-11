86709a9d1aa60284a7974428ae5d9abdbb5d9d1c
dphuong98 <32475252+dphuong98@users.noreply.github.com>
2020-07-11 10:44:50 +0700

    Worker object
    +IsSlacking state
    +Slack temp animation
    +Slack()
    +Deslack()

---
+++ Assets/Scripts/Worker/WorkerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class WorkerControl : MonoBehaviour
+{
+    private bool _isSlacking = false;
+    private bool _slackable = true;
+
+    //Components
+    private Animator _anim;
+
+    public bool IsSlacking()
+    {
+        return _isSlacking;
+    }
+
+    public bool Slackable {
+        get { return _slackable; }
+        set { _slackable = value; }
+    }
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void Slack()
+    {
+        _isSlacking = true;
+        _anim.SetBool("isSlacking", true);
+    }
+
+    public void Deslack()
+    {
+        _isSlacking = false;
+        _anim.SetBool("isSlacking", false);
+    }
+}
